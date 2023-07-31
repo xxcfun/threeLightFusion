9be4e36cde9ec16654a5a1e9fe20d23f214a02ad
xxcfun <echo_dyc@163.com>
2023-07-31 17:32:32 +0800

    init

---
+++ CMakeFiles/cloudVisInfImgFusion.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cloudVisInfImgFusion.dir/main.cpp.o"
+  "CMakeFiles/cloudVisInfImgFusion.dir/main.cpp.o.d"
+  "cloudVisInfImgFusion"
+  "cloudVisInfImgFusion.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cloudVisInfImgFusion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
